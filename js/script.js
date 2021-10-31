6f151cba7cbbc185139c1e4891ebe09747b5cc3e
Nesquicktrue <nesquick.true@gmail.com>
2021-10-31 21:09:41 +0100

    Proof of concept

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 const pole = document.querySelector(".pole");
 const pixelDiv = document.createElement("div");
+const posuvnikVelikosti = document.getElementById("posuvnikVelikosti");
+const vyberBarvu = document.getElementById("vyberBarvu");
+let pocetPixelu;
+let barva;
 
+posuvnikVelikosti.onchange = (e) => {
+    pocetPixelu = e.target.value;
+    pole.replaceChildren("");
+    generujPole(pocetPixelu*pocetPixelu);    
+}
 
-let pocetSloupcu = 16;
-let pocetRadku = 16;
+vyberBarvu.addEventListener("input", zmenBarvu, false);
+vyberBarvu.onchange = (e) => {
+    barva = e.target.value;
+}
 
-function generujPole (pocet) {
+function generujPole(pocet) {
+    pole.style.gridTemplateColumns = `repeat(${pocetPixelu}, 1fr)`
+    pole.style.gridTemplateRows = `repeat(${pocetPixelu}, 1fr)`
+    let sirkaPixelu = 400 / pocetPixelu;
+    console.log(sirkaPixelu);
+
     for (let i = 1; i <= pocet; i++) {
-        pole.appendChild(pixelDiv.cloneNode(true)).classList.add("pixel");
+        pixelDiv.style.width = `${sirkaPixelu}`;
+        pixelDiv.style.height = `${sirkaPixelu}`;
+        pole.appendChild(pixelDiv.cloneNode(true)).addEventListener("mouseover", zmenBarvu);
     }
 };
 
-generujPole(pocetSloupcu * pocetRadku);
+function zmenBarvu () {
+    this.style.backgroundColor = `${barva}`;
+}
+
